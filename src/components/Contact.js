afe1e4e8a4e78e9719b659e2264f20121574ff2f
Revivedaniel <mlg.danielstark@gmail.com>
2022-01-07 23:10:45 -0800

    Testing contact api

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,5 +1,8 @@
 import { useState } from "react";
+import axios from "axios";
+
 const Contact = () => {
+  const [defaultError, setDefaultError] = useState("Please Fill Required Fields!");
   const [form, setForm] = useState({ email: "", name: "", phone: "", msg: "" });
   const [active, setActive] = useState(null);
   const [error, setError] = useState(false);
@@ -11,11 +14,29 @@ const Contact = () => {
   const onSubmit = (e) => {
     e.preventDefault();
     if (email && name && phone && msg) {
-      setSuccess(true);
-      setTimeout(() => {
-        setForm({ email: "", name: "", phone: "", msg: "" });
-        setSuccess(false);
-      }, 2000);
+      axios
+        .post(process.env.CONTACT, {
+          name: name,
+          email: email,
+          message: msg,
+        })
+        .then(function (response) {
+          console.log(response);
+          setSuccess(true);
+          setTimeout(() => {
+            setForm({ email: "", name: "", phone: "", msg: "" });
+            setSuccess(false);
+          }, 2000);
+        })
+        .catch(function (error) {
+          console.log(error);
+          setDefaultError("Something Went Wrong!");
+          setError(true);
+          setTimeout(() => {
+            setDefaultError("Please Fill Required Fields!");
+            setError(false);
+          }, 2000);
+        });
     } else {
       setError(true);
       setTimeout(() => {
@@ -53,7 +74,7 @@ const Contact = () => {
               className="empty_notice"
               style={{ display: error ? "block" : "none" }}
             >
-              <span>Please Fill Required Fields!</span>
+              <span>{defaultError}</span>
             </div>
             {/* */}
             <div className="items_wrap">
@@ -152,7 +173,7 @@ const Contact = () => {
             </h3>
             <p>
               <a className="fn__link" href="mailto:webdevstark@gmail.com">
-              webdevstark@gmail.com
+                webdevstark@gmail.com
               </a>
             </p>
           </div>
