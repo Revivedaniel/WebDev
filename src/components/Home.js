5e5236a2cbd673c796186412b1d836045dd230b4
Revivedaniel <mlg.danielstark@gmail.com>
2023-02-11 17:43:57 -0800

    Updated introduction

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,11 +6,11 @@ const Home = () => {
           {/* Main Title */}
           <div className="resumo_fn_main_title">
             <h3 className="subtitle">Introduction</h3>
-            <h3 className="title">Software Engineer</h3>
+            <h3 className="title">Daniel Stark</h3>
             <p className="desc">
-              I am a patient, hard-working, and dedicated software engineer looking for brand new opportunities.
+            As a Software Engineer with a passion for video games and video game music, I have a strong imagination, great communication skills, a talent for explaining code, and the ability to remain calm under pressure.
             </p>
-            <img src="img/signature.png" alt="image" />
+            <img src="img/signature.webp" alt="image" />
           </div>
           {/* /Main Title */}
         </div>
