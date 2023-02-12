2ad63f6a4d06a04d1372fad8d4a7a6e585ac48bd
Revivedaniel <mlg.danielstark@gmail.com>
2023-02-11 17:45:47 -0800

    Removed some comments

--- src/layouts/Layout.js
+++ src/layouts/Layout.js
@@ -2,7 +2,6 @@ import Head from "next/head";
 import { Fragment, useEffect, useState } from "react";
 import PanelContent from "../components/PanelContent";
 import { aTagClick, customCursor, dataImage, sticky } from "../utilits";
-import Cursor from "./Cursor";
 import Footer from "./Footer";
 import Nav from "./Nav";
 import Triggger from "./Triggger";
@@ -25,38 +24,18 @@ const Layout = ({ children }) => {
         <title>Daniel Stark</title>
       </Head>
       <div className="resumo_fn_wrapper">
-        {/* MODALBOX */}
-        {/* <Modalbox /> */}
-        {/* /MODALBOX */}
         <div className="resumo_fn_content">
-          {/* Main Left Part */}
           <div className="resumo_fn_left">
-            {/* Page */}
             <div className="resumo_fn_page">{children}</div>
-            {/* /Page */}
             <Footer />
           </div>
-          {/* /Main Left Part */}
-          {/* Main Right Part */}
           <div className="resumo_fn_right">
-            {/* Menu Triggger */}
             <Triggger open={() => triggerMenu()} />
-            {/* /Menu Triggger */}
-            {/* Panel Content */}
             <PanelContent />
-            {/* /Panel Content */}
           </div>
-          {/* /Main Right Part */}
         </div>
-        {/* Right Hidden Navigation */}
         <Nav close={() => triggerMenu()} trigger={trigger} />
-        {/* /Right Hidden Navigation */}
-        <Cursor />
       </div>
-      {/* /Wrapper All */}
-      {/* Scripts */}
-      {/*[if lt IE 10]>  <![endif]*/}
-      {/* /Scripts */}
     </Fragment>
   );
 };
